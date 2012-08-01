755246b56777862b1289e1907f33586a75bd9432
Omer Kilic <omerkilic@gmail.com>
2012-08-01 17:01:08 +0000

    Tidying up the I2C module.

--- pi_i2c.c
+++ pi_i2c.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <string.h>
+#include "pihwm.h"
 #include "pi_i2c.h"
 
 
@@ -14,9 +15,7 @@ int i2c_init(char* devname)
 	int fd = open(devname, O_RDWR);
 
 	if ( fd < 0 ){
-		#ifdef DEBUG 
-		printf("[Error] Can't open %s : %s\n", devname, strerror(errno));
-		#endif
+		debug("[%s] Can't open %s : %s\n", __func__, devname, strerror(errno));
 		return -1;
 	} else {
 		return fd;
@@ -27,9 +26,7 @@ int i2c_init(char* devname)
 int i2c_select_device(unsigned int fd, unsigned int addr)
 {
 	if ( ioctl(fd, I2C_SLAVE, addr) < 0 ){
-		#ifdef DEBUG 
-		printf("[Error] Can't select device %s: %s\n", addr, strerror(errno));
-		#endif
+		debug("[%s] Can't select device %s: %s\n", __func__, addr, strerror(errno));
 		return -1;
 	} else {
 		return 1;
@@ -43,13 +40,11 @@ int i2c_write(unsigned int fd, unsigned int addr, unsigned char* data, unsigned 
 
 	if( i2c_select_device(fd, addr) ){
 		if ( write(fd, data, len) != len ) {
-			#ifdef DEBUG 
-			printf("[Error] I2C write (address: 0x%X) failed: %s\n", addr, strerror(errno));
-			#endif
+			debug("[%s] I2C write (address: 0x%X) failed: %s\n", __func__, addr, strerror(errno));
 			return -1;
 		}
 	} else {
-		printf("[Error] Can't select I2C device at address: 0x%X, write failed.", addr);
+		printf("[%s] Can't select I2C device at address: 0x%X, write failed\n", __func__, addr);
 		return -1;
 	}
 
@@ -63,18 +58,14 @@ int i2c_read(unsigned int fd, unsigned int addr, unsigned char* data, unsigned i
 
 	if( i2c_select_device(fd, addr) ){
 		if (read(fd, buf, len) != len) {
-			#ifdef DEBUG 
-			printf("[Error] I2C read failed: %s\n", strerror(errno));
-			#endif
+			debug("[%s]: I2C read (address: 0x%X) failed: %s\n", __func__, addr, strerror(errno));
 			return -1;
 		} else {
 			memcpy(data, buf, len);
 			return 1;
 		}
 	} else {
-		#ifdef DEBUG
-		printf("[Error] Can't select I2C device at address: 0x%X, write failed.", addr);
-		#endif
+		debug("[%s] Can't select I2C device at address: 0x%X, write failed\n", __func__, addr);
 		return -1;
 	}
 }
