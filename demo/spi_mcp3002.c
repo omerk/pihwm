e82e389d68344090fe47d74a875112e5a7acc557
Omer Kilic <omerkilic@gmail.com>
2013-04-03 11:31:30 +0000

    add MCP3002 demo

---
+++ demo/spi_mcp3002.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <linux/types.h>
+
+#include "pihwm.h"
+#include "pi_spi.h"
+
+int
+mcp3002_read (int fd, int channel)
+{
+	uint8_t tx[3];
+	uint8_t rx[3];
+
+	if ( channel == 0 || channel == 1 ){
+		tx[0] = 1; 							// start bit
+		tx[1] = (2 + channel) << 6;	// (sgl/diff) (odd/sign) (msbf)
+												// odd/sign select channel in single ended mode
+		tx[2] = 0;							// dummy data to shift result
+
+		// CH0: 0000 0001  1000 0000  0000 0000
+		// CH1: 0000 0001  1100 0000  0000 0000
+		spi_transfer(fd, tx, rx, 3);
+		
+		return ((rx[1] & 31) << 6) + (rx[2] >> 2);
+	} else {
+		return -1;
+	}
+}
+
+int
+main ()
+{
+	int fd = spi_init(0);
+	int i;
+
+	spi_config_default(fd);
+
+	for( i=0; i< 10; i++){
+		printf("Channel 0: %d, Channel 1: %d\n", mcp3002_read(fd, 0), mcp3002_read(fd, 1));
+		delay(1000);
+	}
+
+	return 0;
+
+}
+
